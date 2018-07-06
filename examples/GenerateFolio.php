3a8b55a3a8cc2fd011f6d9393182810acda2757e
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2018-07-06 16:40:10 +0200

    Updated GenerateFolio example with Customer and Customer Address creation

--- examples/GenerateFolio.php
+++ examples/GenerateFolio.php
@@ -3,7 +3,8 @@
 
 require dirname(__DIR__, 4) . '/vendor/autoload.php';
 
-use FaimMedia\BackOfficeClient;
+use FaimMedia\BackOfficeClient,
+    FaimMedia\BackOfficeClient\CustomerAddressItem;
 
 $client = new BackOfficeClient();
 $client
@@ -11,7 +12,24 @@ $client
 	->setClientId(1)
 	->setClientSecret('fdsfd');
 
+define('US_COUNTRY_ID', 226);
 
-$client->folio()->create([
+// set customer address
+	$customerAddress = new CustomerAddressItem([
+		'address'               => 'Sand Rd.',
+		'address_number'        => 8,
+		'address_number_suffix' => 'A',
+		'zipcode'               => '51000',
+		'city'                  => 'BIKINI BOTTOM',
+		'country_id'            => US_COUNTRY_ID,
+	]);
 
-]);
+// generate customer
+	$customer = $client->customer()->create([
+		'name'              => 'Krusty Krab Ltd.',
+		'payment_method_id' => 5,
+		'customer_address'  => $customerAddress,
+	]);
+
+	var_dump($customer->toArray());
+
