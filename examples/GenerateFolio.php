6276948f62f12177d02b2b6daaa732446b79d480
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2018-07-09 10:46:24 +0200

    Updated GenerateFolio example

--- examples/GenerateFolio.php
+++ examples/GenerateFolio.php
@@ -3,33 +3,28 @@
 
 require dirname(__DIR__, 4) . '/vendor/autoload.php';
 
-use FaimMedia\BackOfficeClient,
-    FaimMedia\BackOfficeClient\CustomerAddressItem;
+use FaimMedia\BackOfficeClient;
+
+define('CLIENT_ID', @$argv[1]);
+define('CLIENT_SECRET', @$argv[2]);
+
+define('CUSTOMER_ID', 11);
+define('CUSTOMER_CONTACT_ID', 11);
 
 $client = new BackOfficeClient();
 $client
 	->setUrl('http://api.backoffice.local')
-	->setClientId(1)
-	->setClientSecret('fdsfd');
+	->setClientId(CLIENT_ID)
+	->setClientSecret(CLIENT_SECRET);
 
 define('US_COUNTRY_ID', 226);
 
-// set customer address
-	$customerAddress = new CustomerAddressItem([
-		'address'               => 'Sand Rd.',
-		'address_number'        => 8,
-		'address_number_suffix' => 'A',
-		'zipcode'               => '51000',
-		'city'                  => 'BIKINI BOTTOM',
-		'country_id'            => US_COUNTRY_ID,
+// generate folio
+	$folio = $client->folio()->create([
+		'customer_id'         => CUSTOMER_ID,
+		'customer_contact_id' => CUSTOMER_CONTACT_ID,
+		'name'                => 'Kruispuntdatabank',
+
 	]);
 
-// generate customer
-	$customer = $client->customer()->create([
-		'name'              => 'Krusty Krab Ltd.',
-		'payment_method_id' => 5,
-		'customer_address'  => $customerAddress,
-	]);
-
-	var_dump($customer->toArray());
-
+	var_dump($folio);
