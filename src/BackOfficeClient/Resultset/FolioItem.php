e47adf502807b83f2b2418dceccf567330501181
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2018-07-09 11:29:07 +0200

    Removed unused validation method

--- src/BackOfficeClient/Resultset/FolioItem.php
+++ src/BackOfficeClient/Resultset/FolioItem.php
@@ -4,8 +4,6 @@ namespace FaimMedia\BackOfficeClient\Resultset;
 
 use FaimMedia\BackOfficeClient\AbstractResult;
 
-use UnexpectedValueException;
-
 class FolioItem extends AbstractResult {
 
 	/**
@@ -15,38 +13,5 @@ class FolioItem extends AbstractResult {
 		return 'folio/folio/';
 	}
 
-	protected function validate($data) {
-
-		/*foreach(['name', 'permission_reminder'] as $field) {
-			if(empty($data[$field])) {
-				throw new UnexpectedValueException('Field `'.$field.'` is required');
-			}
-		}
-
-		foreach(['campaign_defaults', 'contact'] as $field) {
-			if(empty($data[$field]) || !is_array($data[$field])) {
-				throw new UnexpectedValueException('Field `'.$field.'` must be a valid array');
-			}
-		}
-
-		foreach(['company', 'address1', 'city', 'state', 'zip', 'country'] as $field) {
-			if(empty($data['contact'][$field])) {
-				throw new UnexpectedValueException('Field `contact`.`'.$field.'` has an invalid value');
-			}
-		}
-
-		foreach(['from_name', 'from_email', 'subject', 'language'] as $field) {
-			if(empty($data['campaign_defaults'][$field])) {
-				throw new UnexpectedValueException('Field `campaign_defaults`.`'.$field.'` has an invalid value');
-			}
-		}
-
-		if(!filter_var($data['campaign_defaults']['from_email'], FILTER_VALIDATE_EMAIL)) {
-			throw new UnexpectedValueException('Field `campaign_defaults`.`from_email` is not a valid email address');
-		}
-
-		if(!is_bool($data['email_type_option'])) {
-			throw new UnexpectedValueException('Field `email_type_option` must be a boolean');
-		}*/
 	}
 }
