2f6ba967dd305f72265a94314967812a54658de3
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2018-07-10 14:43:23 +0200

    Added generate end-point to FolioItem class

--- src/BackOfficeClient/Resultset/FolioItem.php
+++ src/BackOfficeClient/Resultset/FolioItem.php
@@ -6,7 +6,8 @@ use FaimMedia\BackOfficeClient\AbstractResult,
 	FaimMedia\BackOfficeClient\Resultset\CustomerItem,
 	FaimMedia\BackOfficeClient\Resultset\Customer\ContactItem as CustomerContactItem,
 	FaimMedia\BackOfficeClient\Resultset\Customer\DepartmentItem as CustomerDepartmentItem,
-	FaimMedia\BackOfficeClient\Resultset\Folio\LineItem as FolioLineItem;
+	FaimMedia\BackOfficeClient\Resultset\Folio\LineItem as FolioLineItem,
+	FaimMedia\BackOfficeClient\Resultset\InvoiceItem as InvoiceItem;
 
 class FolioItem extends AbstractResult {
 
@@ -27,5 +28,28 @@ class FolioItem extends AbstractResult {
 			'customer_contact'    => CustomerContactItem::class,
 			'folio_line'          => FolioLineItem::class,
 		];
+	}
+
+	/**
+	 * Custom generate end-point
+	 */
+	public function generate(): InvoiceItem {
+		$this->validateSaved();
+
+		$response = $this->getRequest()->request($this->getUri().'generate.json', 'PUT', [
+			'id' => $this->getId(),
+		]);
+
+		$data = $response['data'];
+
+		$newData = [
+			'id'             => $data['invoice_id'],
+			'invoice_number' => $data['invoice_number'],
+			'folio_id'       => $data['id'],
+		];
+
+		$invoice = new InvoiceItem($newData, $this->getParent());
+
+		return $invoice;
 	}
 }
